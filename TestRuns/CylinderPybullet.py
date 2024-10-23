7ee3fdd262c055e8e9768c18048640bc7b00f618
tarunja1ks <tarunjaiks@gmail.com>
2024-10-22 17:29:44 -0700

    control velocity

--- TestRuns/CylinderPybullet.py
+++ TestRuns/CylinderPybullet.py
@@ -9,21 +9,23 @@ class PybulletEnviorement:
         p.setGravity(0, 0, -9.81)
         self.plane_id = p.loadURDF("plane.urdf")
         self.Cylinder=Cylinder()
-        self.x_goal = p.addUserDebugParameter("X", -10, 10, 0)
-        self.y_goal=p.addUserDebugParameter("Y", -10, 10, 0)
-    
+        self.x_vel = p.addUserDebugParameter("X Velocity", -10, 10, 0)
+        self.y_vel=p.addUserDebugParameter("Y Velocity", -10, 10, 0)
+        self.params={'X_velocity':self.x_vel,'Y_velocity':self.y_vel}
     def run_simulation(self):
         while True:
             p.stepSimulation()
             time.sleep(1./240.)
             self.Cylinder.updatePosition()
-            self.Cylinder.setVelocity(1,1)
+            x_vel=p.readUserDebugParameter(self.params['X_velocity'])
+            y_vel=p.readUserDebugParameter(self.params['Y_velocity'])
+            self.Cylinder.setVelocity(x_vel,y_vel)
         
             
     
         
          
-class Cylinder:
+class Cylinder: 
     def __init__(self):
         cylinder_height=2
         self.cylinder_id=p.loadURDF("cylinder.urdf",basePosition=[0, 0, cylinder_height / 2])
@@ -44,7 +46,7 @@ class PID:
         self.ki=0
         self.kd=0
         self.previoustime=time
-    def updateconstants(self,kp,ki,kd):
+    def update(self,kp,ki,kd):
         self.kp=kp
         self.ki=ki
         self.kd=kd
@@ -55,7 +57,6 @@ class PID:
     
     def calculateVelocity(self,error,time):
         
-
         return self.kp*error
     
     
