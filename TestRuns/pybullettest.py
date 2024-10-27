128876f6cd72f35260bbd4866f1f936b73a1e5d7
tarunja1ks <tarunjaiks@gmail.com>
2024-10-26 18:07:47 -0700

    Merge fixes

--- TestRuns/pybullettest.py
+++ TestRuns/pybullettest.py
@@ -28,7 +28,8 @@ class PyBulletEnvironment:
             p.stepSimulation()
             self.check_button()
             time.sleep(1./240.)
-    '''Function that checks the button state, if the button is pressed, it creates a new cube.'''
+    '''Function that checks the button state, if the button is pressed, it creates a new cube.
+    To extend to extend change XYZ''' 
     def check_button(self):
         current_button_state = p.readUserDebugParameter(self.button_id)
         if current_button_state != self.prev_button_state:
