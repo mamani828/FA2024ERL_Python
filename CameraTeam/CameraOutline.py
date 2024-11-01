afdcfbde5398bdf5fb88ed004f6fb87a901a7d09
Stephen Huang <sth004@ucsd.edu>
2024-11-01 15:23:57 -0700

    Added to camer class

--- CameraTeam/CameraOutline.py
+++ CameraTeam/CameraOutline.py
@@ -4,6 +4,10 @@ import numpy as np
 import time
 import os
 
+# Camera constants
+CAMERA_YAW = 0
+CAMERA_PITCH = -30
+CAMERA_ROLL = 0
 
 class PybulletSim:
     def __init__(self):
@@ -24,25 +28,43 @@ class Robot:
         self.robot_id = None
         self.loading_robot()
 
-    @staticmethod
-    def loading_robot():
+    def loading_robot(self):
         # Set initial coordinates and orientation for r2d2
         racecar_coordinates = [0, 0, 0.5]  # Make sure it's on level ground
         racecar_orientation = [0, 0, 0]  # Neutral orientation (Euler Angles)
         initial_racecar_orientation = p.getQuaternionFromEuler(racecar_orientation)  # Quaternions
         racecar = p.loadURDF("racecar/racecar.urdf", racecar_coordinates, initial_racecar_orientation)
-
+        self.robot_id = racecar # Set robot_id to id returned by loadURDF
 
 class Camera:
-    pass  # Camera Logic will go here
+    def __init__(self, target_robot):
+        self.target_robot = target_robot
+        self.cam_distance = 1.5
+        self.yaw = 0
+        self.pitch = -30  # downward angle for better view of robot
+        self.roll = 0
+
+    def update_view_matrix(self):
+        # Get the robot's position and orientation  
+        robot_pos, robot_orn = p.getBasePositionAndOrientation(self.target_robot.robot_id)
+        
+        # Set camera position behind and above the robot
+        camera_pos = [robot_pos[0] - self.cam_distance, robot_pos[1], robot_pos[2] + 0.5]
+        view_matrix = p.computeViewMatrix(camera_pos, robot_pos, [0, 0, 1])
+        
+        return view_matrix
 
 
 if __name__ == "__main__":
     sim = PybulletSim()
     robot = Robot()
+    camera = Camera(robot)
+
     for _ in range(100000):
         p.stepSimulation()
         time.sleep(1 / 240)  # Slow down the simulation to real time
 
+        view_matrix = camera.update_view_matrix()
+        p.getCameraImage(1920, 1080, viewMatrix=view_matrix)
         # Disconnect when done
     sim.disconnect()
