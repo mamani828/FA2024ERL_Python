5b10494a93e7d3f69b5ced923a818f09982b278e
Marvin Cruz Gomez <cruzmarvin447@gmail.com>
2024-11-04 13:32:08 -0800

    Added the Object class

--- CameraTeam/SensorTest.py
+++ CameraTeam/SensorTest.py
@@ -43,10 +43,33 @@ class Robot:
         initial_racecar_orientation = p.getQuaternionFromEuler(racecar_orientation)  # Quaternions
         racecar = p.loadURDF("racecar/racecar.urdf", racecar_coordinates, initial_racecar_orientation)
         self.robot_id = racecar  # Set robot_id to id returned by loadURDF
+class Object:
+    def __init__(self, coordinates, color):
+        self.color = color
+        self.coordinates = coordinates
+        self.object_id = self.loading_box() #Stroring the object's ID
+    def loading_box(self):
+        half_extents = [0.5, 0.5, 0.5]
+        box_coordinates = self.coordinates
+        box_orientation = [0, 0, 0]
+        initial_box_orientation = p.getQuaternionFromEuler(box_orientation)
 
+        box_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=half_extents)
+        box_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=half_extents, rgbaColor=self.color)
+
+        # Create a multibody and return its ID
+        return p.createMultiBody(baseMass=50, baseCollisionShapeIndex=box_collision_shape, 
+                                 baseVisualShapeIndex=box_visual_shape,
+                                 basePosition=box_coordinates, 
+                                 baseOrientation=initial_box_orientation)
+"""
+@target_object params
+first set of [] coordinates of box & second set of [] are the colors of the box
+"""
 if __name__ == "__main__":
     sim = PybulletSim()
     robot = Robot()
+    target_object = Object([2, 2, 2], [1, 0, 0, 1])  # Spawning box after initializing PybulletSim
 
     camera_config = open_yaml('camera')
     camera = Camera(robot, camera_config)
