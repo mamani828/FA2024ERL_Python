4470e7a04fe847fafccadad4843ecc4e8d791424
Stephen Huang <sth004@ucsd.edu>
2024-11-05 01:41:52 -0800

    Added Lidar and Sensor outline, added some documentation

--- CameraTeam/SensorTest.py
+++ CameraTeam/SensorTest.py
@@ -7,7 +7,9 @@ import yaml
 import sys
 from Sensors import Camera, Lidar
 
-SENSOR_CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config/sensors.yaml')
+SENSOR_CONFIG_PATH = os.path.join(os.path.dirname(__file__),
+                                  'config/sensors.yaml')
+
 
 def open_yaml(config_name):
     try:
@@ -17,15 +19,23 @@ def open_yaml(config_name):
         print("Config file not found!")
         sys.exit(0)
 
-class PybulletSim:
+
+class PybulletEnvironment:
     def __init__(self):
-        self.physics_client = p.connect(p.GUI)  # Connecting to the Physics simulator
-        p.setGravity(0, 0, -9.81)  # Setting the gravity value for the simulation
+        self.physics_client = p.connect(p.GUI)  # Connect to Physx simulator
+        p.setGravity(0, 0, -9.81)  # Setting gravity value for the simulation
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         coordinates = [0, 0, 0]  # initial coordinates and orientation for the plane
         orientation = [0, 0, 0]  # Euler Angles Roll, Pitch, Yaw
         initial_orientation = p.getQuaternionFromEuler(orientation)  # Converting to Quaternion
         self.plane_id = p.loadURDF("plane.urdf", coordinates, initial_orientation)
+
+    def run_simulation(self, *args):
+        while True:
+            p.stepSimulation()
+            time.sleep(1 / 240)
+            # TODO: args is a tuple of sensors. Iterate through this tuple
+            #       and update each sensor
 
     def disconnect(self):
         p.disconnect(self.physics_client)
@@ -41,13 +51,17 @@ class Robot:
         racecar_coordinates = [0, 0, 0.5]  # Make sure it's on level ground
         racecar_orientation = [0, 0, 0]  # Neutral orientation (Euler Angles)
         initial_racecar_orientation = p.getQuaternionFromEuler(racecar_orientation)  # Quaternions
-        racecar = p.loadURDF("racecar/racecar.urdf", racecar_coordinates, initial_racecar_orientation)
+        racecar = p.loadURDF("racecar/racecar.urdf", racecar_coordinates,
+                             initial_racecar_orientation, useFixedBase=True)
         self.robot_id = racecar  # Set robot_id to id returned by loadURDF
+
+
 class Object:
     def __init__(self, coordinates, color):
         self.color = color
         self.coordinates = coordinates
-        self.object_id = self.loading_box() #Stroring the object's ID
+        self.object_id = self.loading_box()  # Storing the object's ID
+
     def loading_box(self):
         half_extents = [0.5, 0.5, 0.5]
         box_coordinates = self.coordinates
@@ -58,21 +72,25 @@ class Object:
         box_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=half_extents, rgbaColor=self.color)
 
         # Create a multibody and return its ID
-        return p.createMultiBody(baseMass=50, baseCollisionShapeIndex=box_collision_shape, 
+        return p.createMultiBody(baseMass=50, baseCollisionShapeIndex=box_collision_shape,
                                  baseVisualShapeIndex=box_visual_shape,
-                                 basePosition=box_coordinates, 
+                                 basePosition=box_coordinates,
                                  baseOrientation=initial_box_orientation)
 """
 @target_object params
 first set of [] coordinates of box & second set of [] are the colors of the box
 """
+
 if __name__ == "__main__":
-    sim = PybulletSim()
+    sim = PybulletEnvironment()
     robot = Robot()
     target_object = Object([2, 2, 2], [1, 0, 0, 1])  # Spawning box after initializing PybulletSim
 
-    camera_config = open_yaml('camera')
+    camera_config = open_yaml("camera_configs")
     camera = Camera(robot, camera_config)
+
+    #  lidar_config = open_yaml("lidar_configs")
+    #  lidar = Lidar(robot, lidar_config)
 
     for _ in range(100000):
         p.stepSimulation()
--- CameraTeam/Sensors.py
+++ CameraTeam/Sensors.py
@@ -1,24 +1,226 @@
 import pybullet as p
 import pybullet_data
 import numpy as np
+import math
+from abc import ABC, abstractmethod
+
+
+class Sensor(ABC):
+    """
+    Sensor abstract base class. ABC acts as a blueprint class,
+    and enforces that all subclasses must have certain methods
+    implemented. If those classes are not implemented, the code
+    will not compile.
+
+    Note: Feel free to add or remove methods in this class. Also,
+    do not use this class until later.
+
+    Args:
+        ABC (Class):
+    """
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    @abstractmethod
+    def get_data(self):
+        """
+        Every sensor class must have a method to obtain data.
+        """
+        pass
+
+    @abstractmethod
+    def update_sensor(self):
+        """
+        Every sensor that needs an update will have an update method.
+        """
+
+    @abstractmethod
+    def attach_to_car(self):
+        """
+        Every sensor must be attached to the car.
+        """
+        pass
+
 
 class Camera:
-    def __init__(self, target_robot, config):
-        self.target_robot = target_robot
-        self.cam_distance = 1.5
-        self.yaw = config['yaw']
-        self.pitch = config['pitch']  
-        self.roll = config['roll']
+    """
+        Camera sensor that is attached to the car. Provides visual feedback
+        as to what the car currently sees.
+    """
+    def __init__(self, target_robot: int, config: dict) -> None:
+        """
+        Initializes the robot camera.
 
-    def update_view_matrix(self):
-        # Get the robot's position and orientation  
+        Args:
+            target_robot (int): car id.
+            config (dict):
+                - camera_distance: Constant int to alter camera location
+                - yaw: y-axis rotation
+                - pitch: x-axis rotation
+                - roll: z-axis rotation
+        """
+        self.CAMERA_DISTANCE = config['camera_distance']
+        self.YAW = config['yaw']
+        self.PITCH = config['pitch']
+        self.ROLL = config['roll']
+
+        # Gets the car ID
+        self.target_robot = target_robot
+
+    def update_sensor(self) -> list:
+        """
+        Computes the current view_matrix of the camera.
+
+        Args:
+            None.
+        Returns:
+            view_matrix:
+        Raises:
+            None.
+        """
+        # Get the robot's position and orientation
         robot_pos, _ = p.getBasePositionAndOrientation(self.target_robot.robot_id)
-        
+
         # Camera is behind and above robot
-        camera_pos = [robot_pos[0] - self.cam_distance, robot_pos[1], robot_pos[2] + 0.5]
+        camera_pos = [robot_pos[0] - self.CAMERA_DISTANCE, robot_pos[1], robot_pos[2] + 0.5]
         view_matrix = p.computeViewMatrix(camera_pos, robot_pos, [0, 0, 1])
-        
+
         return view_matrix
 
+
+#  Note: Imported from main GitHub. Modify as needed.
 class Lidar:
-    pass
+    """
+    Light radar class. Uses Pybullet's rayTestBatch.
+    """
+    def __init__(self, target_robot, config) -> None:
+        self.target_robot = target_robot
+
+        self.LIDAR_JOINTS = config['lidar_joints']
+        self.START_ANGLE = config['lidar_angle1']
+        self.END_ANGLE = config['lidar_angle2']
+        self.NUM_RAYS = config['num_rays']
+        self.RAY_START_LEN = config['ray_start_len']
+        self.RAY_LEN = config['ray_len']
+        self.HIT_COLOR = config['hit_color']
+        self.MISS_COLOR = config['miss_color']
+
+    def setup(self) -> None:
+        """
+        Setups Lidar object (a one-time setup).
+
+        Args:
+            None.
+        Returns:
+            None.
+        Raises:
+            None.
+        """
+        a = self.START_ANGLE*(math.pi/180)
+        b = (self.END_ANGLE - self.START_ANGLE)*(math.pi/180)
+
+        ray_from, ray_to = [], []
+        for i in range(self.NUM_RAYS):
+            theta = float(a) + (float(b) * (float(i)/self.NUM_RAYS))
+            x1 = self.RAY_START_LEN*math.sin(theta)
+            y1 = self.RAY_START_LEN*math.cos(theta)
+            z1 = 0
+
+            x2 = self.RAY_LEN*math.sin(theta)
+            y2 = self.RAY_LEN*math.cos(theta)
+            z2 = 0
+
+            ray_from.append([x1, y1, z1])
+            ray_to.append([x2, y2, z2])
+
+        self.ray_from = ray_from
+        self.ray_to = ray_to
+
+        ray_ids = []
+        for i in range(self.NUM_RAYS):
+            ray_ids.append(
+                    p.addUserDebugLine(
+                            self.ray_from[i],
+                            self.ray_to[i],
+                            self.MISS_COLOR,
+                            parentObjectUniqueId=self.car_id,
+                            parentLinkIndex=self.LIDAR_JOINTS
+                        )
+                    )
+        self.ray_ids = ray_ids
+
+    def retrieve_data(self, common=True, robot_state=None) -> tuple:
+        """
+        In most mobile robot applicatoins, each ray (or lidar)
+        gives two information: 1. range, 2. bearing (w.r.t car's
+        yaw). Range is how far the ray reached - if it hits something
+        than it should be less than it's max length. Bearing tells
+        us where this scan occurs in the map w.r.t the car's frame.
+
+        However, PyBullet has nicely given us the coordinates in
+        the world frame of obstacles that a ray hits.
+
+        Hence, this function will implement both cases. Set
+        common=False for the second case (since the first case
+        is more common in mobile robots problems)
+
+        Args:
+            robot_state: The car's current state (x, y, yaw). Some sensors
+                       need this, some don't. It is included
+                       for consistency of the API.
+            common: For API consistency, same as `robot_state`.
+        Returns:
+            rays_data: A numpy array needed to simulate LiDAR.
+            coords: Coordinate (x, y) of hit points in world
+                    coordinate. For a ray that does not hit an object,
+                    the value is None.
+            dists: Distance to hit object for every ray. For a ray that
+                   does not hit an object, the value is self.RAY_LEN
+
+        """
+        num_threads = 0
+        rays_data = p.rayTestBatch(
+                    self.ray_from,
+                    self.ray_to,
+                    num_threads,
+                    parentObjectUniqueId=self.car_id,
+                    parentLinkIndex=self.LIDAR_JOINTS
+                )
+
+        # Convert `results` to numpy array to leverage Numpy's speed.
+        rays_data = np.array(rays_data, dtype=object)
+
+        # Get the rays which does not hit an object
+        no_hit = np.where(rays_data[:, 2] == 1.)[0]
+
+        # Convert `coords` to numpy array to leverage Numpy's speed.
+        # `coords` will be of shape (NUM_RAYS, 3) and coords[i] contains
+        # [x, y, z] coordinates in world coordinate, for each rays.
+        coords = np.array(rays_data[:, 3], dtype=object)  # shape: (NUM_RAYS, )
+        coords = np.stack(coords).astype(np.float32)      # shape: (NUM_RAYS, 3)
+        coords = coords[:, :2]  # We only need (x, y) coords.
+
+        # Get the distances
+        x, y, yaw = robot_state
+        dists = np.sqrt((coords[:, 0] - x)**2 + (coords[:, 1] - y)**2)  # (NUM_RAYS, )
+
+        # Set distances to self.RAY_LEN for rays that doesn't hit an object.
+        dists[no_hit] = self.RAY_LEN
+
+        # Get bearing data
+        angle = np.arctan2(coords[:, 1] - y, coords[:, 0] - x)
+        angle *= 180.0/np.pi
+        bearings = angle - yaw
+        bearings = np.where(bearings < 0, bearings + 360, bearings)
+
+        if common:
+            return rays_data, dists, bearings
+
+        # set coord to None for rays with no hit.
+        coords[no_hit] = None  # Set coord to None for rays with not hit.
+        return rays_data, dists, coords
